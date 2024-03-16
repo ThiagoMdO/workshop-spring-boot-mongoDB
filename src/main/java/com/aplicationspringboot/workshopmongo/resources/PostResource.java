e226837d1b26c46365b5dba083b28e85cad6db15
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2024-03-15 21:46:48 -0300

    Obtendo um post por id

--- src/main/java/com/aplicationspringboot/workshopmongo/resources/PostResource.java
+++ src/main/java/com/aplicationspringboot/workshopmongo/resources/PostResource.java
@@ -5,18 +5,19 @@ import com.aplicationspringboot.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/posts")
+@RequestMapping(value = "/posts")
 public class PostResource {
 
     @Autowired
     private PostService postService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<PostDTO> findPostById(String id){
+    public ResponseEntity<PostDTO> findPostById(@PathVariable String id){
         PostDTO postDTO = postService.findPost(id);
         return ResponseEntity.ok().body(postDTO);
     }
